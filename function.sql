485e3aec3867384bffd51750ca315414744649f5
thiennguyenqn <44944761+thiennguyenqn@users.noreply.github.com>
2019-05-12 11:09:12 +0700

    Create function.sql

---
+++ function.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION fuThanhTien(
+@giaban int,
+@soluong int,
+@mucgiamgia dec)
+RETURNS dec
+AS
+BEGIN
+	RETURN @giaban*@soluong-@mucgiamgia;
+END;
+--					         @giaban @sl @mgg  tên cột khi xuất kết quả
+SELECT dbo.fuThanhTien(20000, 10, 0.1) Thanhtien
