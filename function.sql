b7b6345d2132bc0fedb23bf63033e08c76b8a4f0
thiennguyenqn <44944761+thiennguyenqn@users.noreply.github.com>
2019-05-12 11:10:18 +0700

    Update function.sql

--- function.sql
+++ function.sql
@@ -7,5 +7,5 @@ AS
 BEGIN
 	RETURN @giaban*@soluong-@mucgiamgia;
 END;
---					 @giaban @sl @mgg  tên cột khi xuất kết quả
+--		@giaban @sl @mgg  tên cột khi xuất kết quả
 SELECT dbo.fuThanhTien(20000, 10, 0.1) Thanhtien
